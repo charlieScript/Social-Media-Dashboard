366385852b90ec9bd6412ca3314699693b224e52
charieScript <gozione3@gmail.com>
2020-05-18 14:34:15 +0100

    added localstorage for the dark mode option

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
 const toggler = document.querySelector('.switch #check');
 
+// current theme
+const currentTheme = localStorage.getItem('theme');
+
+// check for current theme 
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+    if (currentTheme === 'dark') {
+        toggler.checked = true;
+    }
+}
+
 toggler.addEventListener('input', (e) => {
     if (e.target.checked) {
         document.documentElement.setAttribute('data-theme', 'dark');
+        // local storgae
+        localStorage.setItem('theme', 'dark')
     } else {
         document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light')
     }
 })
 
